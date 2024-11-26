23d65d6ba339363a8081caafb74e774dbe7cfe8e
mauzer3358 <mauzer3358@gmail.com>
2024-11-26 22:38:06 +0300

    Add files via upload

--- Module_16_4.py
+++ Module_16_4.py
@@ -19,11 +19,12 @@ def create_user(user: Users, username: str, age: int) -> str:
     if user.id is None:
         user.id = 1
     else:
-        user.id = len(users)+1
+        user.id = max((t.id for t in users), default=0) + 1 #max(id_list, default=0) + 1
     user.username = username
     user.age = age
-
     users.append(user)
+    for t in user:
+        print(t)
     return f"User {username} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
@@ -38,8 +39,14 @@ def update_users(user_id: int, username: str, age = int) ->str:
 
 @app.delete("/user/{user_id}")
 def delete_message(user_id: int) -> str:
-    try:
-        users.pop(user_id-1)
+    if user_id in [t.id for t in users]:
+        count = 0
+        for i in users:
+            if i.id == user_id:
+                break
+            else:
+                count+=1
+        print('True')
+        users.pop(count)
         return f"Message ID={user_id} deleted!"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+    else: raise HTTPException(status_code=404, detail="User was not found")
